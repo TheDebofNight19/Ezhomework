39946dbf632281f269caa73856f776b43d937ec9
EZvyagintsev <zvyagintsev.e@gmail.com>
2020-03-23 19:12:01 +0300

    Удалил папку .idea из репозитория

---
+++ src/lesson6/Basket/ShopBasket.java
@@ -0,0 +1,44 @@
+package lesson6.Basket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShopBasket implements Basket {
+
+
+    @Override
+    public void addProduct(String product, int quantity) {
+
+        ArrayList<String> products = new ArrayList();
+
+
+        }
+
+
+
+
+    @Override
+    public void removeProduct(String product) {
+
+    }
+
+    @Override
+    public void updateProductQuantity(String product, int quantity) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public List<String> getProducts() {
+        return null;
+    }
+
+    @Override
+    public int getProductQuantity(String product) {
+        return 0;
+    }
+}
